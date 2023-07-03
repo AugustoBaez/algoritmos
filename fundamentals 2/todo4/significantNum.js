2f3814355b8880fb88140980482f73d7c99701e7
augusto <asbaez@outlook.com>
2023-07-03 14:54:42 -0400

    add methods

---
+++ fundamentals 2/todo4/significantNum.js
@@ -0,0 +1,18 @@
+function significantNum(num) {
+
+    let number = num.map(function (x, index) {
+        if (x[index] !== 0) {
+            return x
+        }
+    })
+
+    let i = 0
+    while () {
+
+    }
+
+    console.log(number)
+
+}
+
+significantNum(321)
--- linked lists/first.js
+++ linked lists/first.js
@@ -48,6 +48,68 @@ class SLL {
         }
         console.log(`hay ${count} nodos`)
     }
+    max() {
+        if (this.head == null) {
+            return null
+        }
+        let currentNode = this.head
+        let maxNode = currentNode
+        while (currentNode) {
+            if (currentNode.value > maxNode.value) {
+                maxNode = currentNode
+            }
+            currentNode = currentNode.next
+        }
+        console.log("el valor maximo es: ", maxNode.value)
+    }
+    min() {
+        if (this.head == null) {
+            console.log("la lista esta vacia")
+            return null
+        }
+        let currentNode = this.head
+        let minNode = currentNode.value
+        while (currentNode) {
+            if (currentNode.value < minNode) {
+                minNode = currentNode.value
+            }
+            currentNode = currentNode.next
+        }
+        console.log("el valor minimo es: ", minNode)
+        return minNode
+    }
+    average() {
+        if (this.head == null) {
+            console.log("La lista está vacía.")
+            return null
+        }
+        let nodeCount = null
+        let currentNode = this.head
+        let totalNodesValues = 0
+        while (currentNode) {
+            totalNodesValues += currentNode.value;
+            nodeCount++
+            currentNode = currentNode.next
+        }
+        let average = totalNodesValues / nodeCount
+        console.log("el valor promedio es: ", average)
+        return average
+    }
+    back() {
+        if (this.head == null) {
+            console.log("La lista está vacía.")
+            return null
+        }
+
+        let currentNode = this.head
+        while (currentNode.next) {
+            currentNode = currentNode.next
+        }
+
+        const lastValue = currentNode.value
+        console.log("El último valor de la lista es:", lastValue)
+        return lastValue
+    }
     display() {
         let currentNode = this.head
         while (currentNode) {
@@ -64,4 +126,8 @@ myList.removeFront()
 myList.front()
 myList.contains(5)
 myList.length()
+myList.max()
+myList.min()
+myList.average()
+myList.back()
 myList.display()
